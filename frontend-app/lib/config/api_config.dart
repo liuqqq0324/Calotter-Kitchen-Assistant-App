9d4e7972114b584f1a1020ccd8a550af4549a64b
liuqqq0324 <Liuqi0324@gmail.com>
2026-01-21 18:42:31 +1300

    modifying homepage UI

--- frontend-app/lib/config/api_config.dart
+++ frontend-app/lib/config/api_config.dart
@@ -11,8 +11,8 @@ class ApiConfig {
   // 如果用真机，请填你电脑的局域网 IP (如 "192.168.1.100")
 
   //static const String serverIp = "16.176.170.214"; // IP 换成AWS 公网 IP
-  //static const String serverIp = "10.0.2.2"; // Android 模拟器使用此 IP
-  static const String serverIp = "172.23.83.55"; // 真机调试时使用此 IP（当前本机 IP）
+  static const String serverIp = "10.0.2.2"; // Android 模拟器使用此 IP
+  // static const String serverIp = "172.24.2.170"; // 真机调试时使用此 IP
 
   // ⚠️ 后端是单体应用，所有服务都在 8080 端口
   // Java 后端端口 (查看 Java 项目终端显示的 http://0.0.0.0:xxxx)
